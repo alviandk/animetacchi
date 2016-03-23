5d2cdaa9efa3c3c2824cf32365a21e470afc1a95
alvian <alviandk@gmail.com>
2016-03-23 12:30:01 +0700

    db t sqlite

--- animetacchi/settings.py
+++ animetacchi/settings.py
@@ -12,15 +12,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'animetacchi',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'recruit',
-        'PASSWORD': 'recruit',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+        'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': os.path.join(PROJECT_PATH, 'db.sqlite3'),
+                }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
