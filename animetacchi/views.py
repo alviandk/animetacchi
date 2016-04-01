b3a8c44f5927a8da31ffaaf5143e538266eca0c6
alvian <alviandk@gmail.com>
2016-04-01 23:12:40 +0700

    confirmation

--- animetacchi/views.py
+++ animetacchi/views.py
@@ -155,7 +155,7 @@ def register_confirm(request, activation_key):
 
     #check if the activation key has expired, if it hase then render confirm_expired.html
     if user_profile.key_expires + timedelta(days=1) < timezone.now():
-        user_profile.user.delete()
+        #user_profile.user.delete()
         return render_to_response('User/confirm_expired.html',locals(),context_instance=RequestContext(request))
     #if the key hasn't expired save user and set him as active and render some template to confirm activation
     user = user_profile.user
